096809a3aa55bd60a755e72f536ce90806df7f9c
rejaur-rahman@hotmail.co.uk <rejaur-rahman@hotmail.co.uk>
2018-12-17 01:05:29 +0000

    discover zone section blocks added

--- src/views/DiscoverZone.vue
+++ src/views/DiscoverZone.vue
@@ -1,6 +1,54 @@
 <template>
   <div class="body-container">
-    <h1>This is discover zone page</h1>
+  	<div class="container">
+  		<h1>discover zone</h1>
+	  	<div class="discover-more-block-container">
+	  		<div class="discover-more-block">
+	  			<nav class="panel">
+					  <p class="panel-heading">freshwater zone</p>
+					  <div class="panel-block">
+					  	<img src="../assets/images/zones/freshwater.jpg">
+					  </div>
+					  <div class="panel-block panel-footer">
+					    <button class="button is-link is-outlined is-fullwidth">go to zone</button>
+					  </div>
+					</nav>
+	  		</div>
+	  		<div class="discover-more-block">
+	  			<nav class="panel">
+					  <p class="panel-heading">ocean zone</p>
+					  <div class="panel-block">
+					  	<img src="../assets/images/zones/ocean.jpg">
+					  </div>
+					  <div class="panel-block panel-footer">
+					    <button class="button is-link is-outlined is-fullwidth">go to zone</button>
+					  </div>
+					</nav>
+	  		</div>
+	  		<div class="discover-more-block">
+	  			<nav class="panel">
+					  <p class="panel-heading">saltwater zone</p>
+					  <div class="panel-block">
+					  	<img src="../assets/images/zones/saltwater.jpg">
+					  </div>
+					  <div class="panel-block panel-footer">
+					    <button class="button is-link is-outlined is-fullwidth">go to zone</button>
+					  </div>
+					</nav>
+	  		</div>
+	  		<div class="discover-more-block">
+	  			<nav class="panel">
+					  <p class="panel-heading">shark zone</p>
+					  <div class="panel-block">
+							<img src="../assets/images/zones/shark.jpg">
+					  </div>
+					  <div class="panel-block panel-footer">
+					    <button class="button is-link is-outlined is-fullwidth">go to zone</button>
+					  </div>
+					</nav>
+	  		</div>
+	  	</div>
+	  </div>
   </div>
 </template>
 
@@ -50,5 +98,71 @@
 <style scoped lang="scss">
   .body-container {
     padding-top: 105px;
+    h1 {
+    	padding-top: 50px;
+    	padding-bottom: 25px;
+    	padding-left: 12px;
+    	padding-right: 12px;
+			font-size: 32px;
+	    line-height: 1.2;
+	    text-transform: uppercase;
+	    font-weight: bold;
+	    margin: 0px;
+	    letter-spacing: 0.06em;
+	    color: #000;
+	    text-align: left;
+  	}
+    .discover-more-block-container {
+    	.discover-more-block {
+    		margin: 0px 0px 50px 0px;
+    		display: inline-block;
+    		width: 25%;
+    		@media (min-width: 640px) and (max-width: 1199px) {
+    			width: 50%;
+    		}
+    		@media (max-width: 639px) {
+    			width: 100%;
+    		}
+    		.panel {
+    			padding: 0px 12px;
+    			.panel-heading {
+    				background: none;
+    				border: 1px solid #28817f;
+    				text-transform: uppercase;
+    				color: #363636;
+    				padding-top: 0px;
+    				padding-bottom: 0px;
+    				line-height: 46.8px;
+    				border-top-left-radius: 8px;
+    				border-top-right-radius: 8px;
+    			}
+    			.panel-block {
+    				padding: 0px;
+    				border: 1px solid #28817f;
+    				border-top: none;
+    				&.panel-footer {
+    					border-bottom-left-radius: 8px;
+    					border-bottom-right-radius: 8px;
+    					&:hover {
+    						background: #28817f;
+    					}
+    				}
+    				button {
+    					border: none;
+    					border-radius: 0px;
+    					text-transform: uppercase;
+    					color: #363636;
+    					&:hover {
+    						background: none;
+    						color: #FFF;
+    					}
+    					&:focus {
+    						background: none;
+    					}
+    				}
+    			}
+    		}
+    	}
+  	}
   }
 </style>
