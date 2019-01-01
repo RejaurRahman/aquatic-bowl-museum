476409f0f0cb438249128081eae7b0d8d894be43
rejaur-rahman@hotmail.co.uk <rejaur-rahman@hotmail.co.uk>
2019-01-01 23:34:40 +0000

    added freshwater species data

---
+++ src/assets/js/freshwater-data.js
@@ -0,0 +1,78 @@
+var freshwaters =
+{
+	"Arapaima":
+	{
+		heading: "Arapaima",
+		description: "",
+		fishIcon: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: ""
+	},
+	"BelugaSturgeon":
+	{
+		heading: "Beluga Sturgeon",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: ""
+	},
+	"ChinookSalmon":
+	{
+		heading: "Chinook Salmon",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: ""
+	},
+	"GoldFish":
+	{
+		heading: "Gold Fish",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: ""
+	},
+	"KoiCarp":
+	{
+		heading: "Koi Carp",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: "",
+	},
+	"RedBelliedPiranha":
+	{
+		heading: "Red-Bellied Piranha",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: "",
+	},
+	"RedtailCatfish":
+	{
+		heading: "Redtail Catfish",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: "",
+	},
+	"Sawfish":
+	{
+		heading: "Sawfish",
+		description: "",
+		specieTwitterLink: "",
+		specieFaceBookLink: "",
+		specieLinkedInLink: "",
+		specieMailLink: "",
+	},
+}
+
+export default freshwaters
